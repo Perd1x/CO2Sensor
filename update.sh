34621c9da7008e5302e5c3106ed82fc5a058a8ce
Perd1x <81868139+Perd1x@users.noreply.github.com>
2021-05-07 17:42:42 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd /home/flo/Desktop/CO2Sensor/BackupRepo/CO2Sensor-src
+cd /home/pi/Desktop/CO2Sensor/BackupRepo/CO2Sensor-src
 sudo git pull
-cd /home/flo/Desktop/CO2Sensor
+cd /home/pi/Desktop/CO2Sensor
 sudo rm -r ./CO2Sensor-src
 sudo cp -r ./BackupRepo/CO2Sensor-src ./
 #sudo sh ./CO2Sensor-src/start.sh
