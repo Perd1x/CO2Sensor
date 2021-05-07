9cd988f265ec0d289a9096a3399459a160cc95da
Perd1x <81868139+Perd1x@users.noreply.github.com>
2021-05-07 17:42:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #this programm will install the CO2Sensor software by Perdix
 
-cd /home/flo/Desktop/
+cd /home/pi/Desktop/
 
 #create needet folders & create backup & move update.sh
 mkdir ./CO2Sensor
 mkdir ./CO2Sensor/BackupRepo
 echo 'created directories'
-sudo cp -r /home/flo/Documents/CO2Sensor-src ./CO2Sensor
-sudo cp -r /home/flo/Documents/CO2Sensor-src ./CO2Sensor/BackupRepo
+sudo cp -r /home/pi/Documents/CO2Sensor-src ./CO2Sensor
+sudo cp -r /home/pi/Documents/CO2Sensor-src ./CO2Sensor/BackupRepo
 echo 'copied git repositorie'
 sudo cp ./CO2Sensor/CO2Sensor-src/update.sh ./CO2Sensor
 echo 'moved update.sh'
